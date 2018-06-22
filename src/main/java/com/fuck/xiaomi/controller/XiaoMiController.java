8269947b53ef6ce4e8cbdff43d085a23059dec71
liwei <1038084585@qq.com>
2018-06-22 11:41:03 +0800

    dd

--- src/main/java/com/fuck/xiaomi/controller/XiaoMiController.java
+++ src/main/java/com/fuck/xiaomi/controller/XiaoMiController.java
@@ -1,9 +1,6 @@
 package com.fuck.xiaomi.controller;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.alibaba.fastjson.JSON;
 import com.fuck.xiaomi.annotation.Controller;
 import com.fuck.xiaomi.annotation.Resource;
